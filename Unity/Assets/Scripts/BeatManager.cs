5428d3005986cc8d9813681eb035509f7442338f
Kev Adsett <kev.adsett@gmail.com>
2017-11-04 10:09:49 +0000

    Set up test scene using G-Audio

---
+++ Unity/Assets/Scripts/BeatManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BeatManager : MonoBehaviour
+{
+	public int BPM = 120;
+
+	public int BeatsPerBar = 4;
+
+	public bool ShouldPlay = true;
+
+	public float Delay = 0.5f;
+
+	public delegate void BeatOccurredAction(int beatNumber, int barNumber);
+	public static event BeatOccurredAction OnCrotchetOccurred;
+
+	private const float DIVISOR = 8f;
+	private float _tickCount = 1f;
+
+	private int _barCount = 1;
+
+	void Start ()
+	{
+		StartCoroutine (DoBeat ());
+	}
+
+	private IEnumerator DoBeat()
+	{
+		yield return new WaitForSeconds (Delay);
+		while (true)
+		{
+			if (ShouldPlay)
+			{
+				Debug.Log ("[" + _barCount + ": " + _tickCount + "]");
+				if (_tickCount % 1 == 0)
+				{
+					if (OnCrotchetOccurred != null)
+					{
+						OnCrotchetOccurred ((int)_tickCount, _barCount);
+					}
+
+				}
+				_tickCount += (1 / DIVISOR);
+				if (_tickCount == BeatsPerBar + 1)
+				{
+					_tickCount = 1f;
+					_barCount++;
+				}
+			}
+			yield return new WaitForSeconds (60f / (float)BPM / DIVISOR);
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/RandomNotePlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using GAudio;
+
+public class RandomNotePlayer : MonoBehaviour
+{
+	public GATSampleBank SampleBank;
+
+	public BeatManager BeatManager;
+
+	private string[] _sampleNames
+	{
+		get {
+			return SampleBank.AllSampleNames;
+		}
+	}
+
+	void OnEnable ()
+	{
+		BeatManager.OnCrotchetOccurred += OnBeatOccurred;
+	}
+
+	void Disable()
+	{
+		BeatManager.OnCrotchetOccurred -= OnBeatOccurred;
+	}
+
+	void OnBeatOccurred (int beatCount, int barCount)
+	{
+		GATData sampleData = SampleBank.GetAudioData (_sampleNames [Random.Range(0, _sampleNames.Length)]);
+		GATManager.DefaultPlayer.PlayData (sampleData, 0, 0.2f);
+	}
+}
